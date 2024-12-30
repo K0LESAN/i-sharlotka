3fbc0f1a64416d2e52e224c1ce60a10e6c0ce04e
K0LESAN <k0lesan2k00@gmail.com>
2024-12-30 17:50:56 +0300

    feat: add fonts and color variables

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,156 @@
+import { Open_Sans, Montserrat, Roboto, Raleway } from 'next/font/google';
+import localFont from 'next/font/local';
 import type { Metadata } from 'next';
-import './globals.css';
+import './globals.scss';
+
+const openSans = Open_Sans({
+  weight: ['400', '600', '700'],
+  preload: false,
+  variable: '--open-sans',
+  fallback: ['sans-serif']
+});
+const montserrat = Montserrat({
+  weight: ['400', '900'],
+  preload: false,
+  variable: '--montserrat',
+  fallback: ['sans-serif']
+});
+const roboto = Roboto({
+  weight: ['400', '700'],
+  preload: false,
+  variable: '--roboto',
+  fallback: ['sans-serif']
+});
+const raleway = Raleway({
+  weight: '400',
+  preload: false,
+  variable: '--raleway',
+  fallback: ['sans-serif']
+});
+const gilroy = localFont({
+  src: [
+    {
+      path: './fonts/gilroy/gilroy-ultra-light.eot',
+      weight: '200'
+    },
+    {
+      path: './fonts/gilroy/gilroy-ultra-light.ttf',
+      weight: '200'
+    },
+    {
+      path: './fonts/gilroy/gilroy-ultra-light.woff',
+      weight: '200'
+    },
+    {
+      path: './fonts/gilroy/gilroy-light.eot',
+      weight: '300'
+    },
+    {
+      path: './fonts/gilroy/gilroy-light.ttf',
+      weight: '300'
+    },
+    {
+      path: './fonts/gilroy/gilroy-light.woff',
+      weight: '300'
+    },
+    {
+      path: './fonts/gilroy/gilroy-regular.eot',
+      weight: '400'
+    },
+    {
+      path: './fonts/gilroy/gilroy-regular.ttf',
+      weight: '400'
+    },
+    {
+      path: './fonts/gilroy/gilroy-regular.woff',
+      weight: '400'
+    },
+    {
+      path: './fonts/gilroy/gilroy-medium.eot',
+      weight: '500'
+    },
+    {
+      path: './fonts/gilroy/gilroy-medium.ttf',
+      weight: '500'
+    },
+    {
+      path: './fonts/gilroy/gilroy-medium.woff',
+      weight: '500'
+    },
+    {
+      path: './fonts/gilroy/gilroy-semi-bold.eot',
+      weight: '600'
+    },
+    {
+      path: './fonts/gilroy/gilroy-semi-bold.ttf',
+      weight: '600'
+    },
+    {
+      path: './fonts/gilroy/gilroy-semi-bold.woff',
+      weight: '600'
+    },
+    {
+      path: './fonts/gilroy/gilroy-bold.eot',
+      weight: '700'
+    },
+    {
+      path: './fonts/gilroy/gilroy-bold.ttf',
+      weight: '700'
+    },
+    {
+      path: './fonts/gilroy/gilroy-bold.woff',
+      weight: '700'
+    },
+    {
+      path: './fonts/gilroy/gilroy-extra-bold.eot',
+      weight: '800'
+    },
+    {
+      path: './fonts/gilroy/gilroy-extra-bold.ttf',
+      weight: '800'
+    },
+    {
+      path: './fonts/gilroy/gilroy-extra-bold.woff',
+      weight: '800'
+    },
+    {
+      path: './fonts/gilroy/gilroy-heavy.eot',
+      weight: '900'
+    },
+    {
+      path: './fonts/gilroy/gilroy-heavy.ttf',
+      weight: '900'
+    },
+    {
+      path: './fonts/gilroy/gilroy-heavy.woff',
+      weight: '900'
+    }
+  ],
+  variable: '--gilroy',
+  fallback: ['sans-serif']
+});
+const akrobat = localFont({
+  src: [
+    {
+      path: './fonts/akrobat/akrobat-regular.eot',
+      weight: '400'
+    },
+    {
+      path: './fonts/akrobat/akrobat-regular.otf',
+      weight: '400'
+    },
+    {
+      path: './fonts/akrobat/akrobat-regular.ttf',
+      weight: '400'
+    },
+    {
+      path: './fonts/akrobat/akrobat-regular.woff',
+      weight: '400'
+    }
+  ],
+  variable: '--akrobat',
+  fallback: ['sans-serif']
+});
 
 export const metadata: Metadata = {
   title: 'iSharlotka',
@@ -13,7 +164,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='ru'>
-      <body>{children}</body>
+      <body
+        className={`${openSans.variable} ${montserrat.variable} ${raleway.variable} ${roboto.variable} ${gilroy.variable} ${akrobat.variable}`}>
+        {children}
+      </body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,15 +2,30 @@ import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
+      fontFamily: {
+        montserrat: ['var(--montserrat)'],
+        gilroy: ['var(--gilroy)'],
+        akrobat: ['var(--akrobat)'],
+        roboto: ['var(--roboto)'],
+        raleway: ['var(--raleway)'],
+        'open-sans': ['var(--open-sans)']
+      },
       colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)'
+        lightborder: 'var(--lightborder)',
+        gray: 'var(--gray)',
+        'gray-dark': 'var(--gray)',
+        'blue-dark': 'var(--blue)',
+        'blue-light': 'var(--blue)',
+        'gray-text': 'var(--gray)',
+        'gray-medium': 'var(--gray)',
+        green: 'var(--green)',
+        red: 'var(--red)',
+        white: 'var(--white)'
       }
     }
   },
