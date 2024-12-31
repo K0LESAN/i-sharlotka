c5cda62b7aa47acabf94011341da6587d9bdc406
K0LESAN <k0lesan2k00@gmail.com>
2024-12-31 04:30:54 +0300

    fix: correct redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
 import { checkLocale } from '@/shared/utils';
 import { getLocale } from 'next-intl/server';
-import { notFound, redirect, RedirectType } from 'next/navigation';
+import { notFound, RedirectType } from 'next/navigation';
+import { redirect } from '@/i18n/routing';
 
 export default async function RootPage() {
   const locale: string = await getLocale();
 
   if (checkLocale(locale)) {
-    redirect(`/${locale}`, RedirectType.replace);
+    redirect(
+      {
+        href: '/',
+        locale
+      },
+      RedirectType.replace
+    );
   } else {
     notFound();
   }
