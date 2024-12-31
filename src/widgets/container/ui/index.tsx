97fbfbd5a9d4ed798aef35d8f0b43a2140e66279
K0LESAN <k0lesan2k00@gmail.com>
2024-12-31 07:17:06 +0300

    feat: add container

---
+++ src/widgets/container/ui/index.tsx
@@ -0,0 +1,9 @@
+import styles from './index.module.scss';
+
+export default async function Container({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return <div className={styles.container}>{children}</div>;
+}
