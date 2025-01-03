70ec9cccfd2b43acf140116ee862bcdad9e53493
K0LESAN <k0lesan2k00@gmail.com>
2025-01-03 14:31:46 +0300

    feat: add constants

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,7 +1,23 @@
-type DefaultLocale = 'ru';
-type Locales = DefaultLocale | 'en';
+import type { Env } from '@/types/env';
+import type { DefaultLocale, Locales } from '@/types/localization';
 
 const locales: Locales[] = ['ru', 'en'];
 const defaultLocale: DefaultLocale = 'ru';
 
-export { locales, defaultLocale, type Locales, type DefaultLocale };
+const env: Env = {
+  PAYLOAD_CMS_URL: process.env.PAYLOAD_CMS_URL || '',
+  BASE_URL: process.env.BASE_URL || ''
+};
+
+enum Endpoints {
+  Cases = '/cases'
+}
+
+export {
+  Endpoints,
+  locales,
+  defaultLocale,
+  env,
+  type Locales,
+  type DefaultLocale
+};
