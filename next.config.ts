de7568cbedd488c2ac276da23a810e56f638db6d
K0LESAN <k0lesan2k00@gmail.com>
2025-01-03 14:27:22 +0300

    feat: add new env

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
+  env: {
+    PAYLOAD_CMS_URL: process.env.PAYLOAD_CMS_URL,
+    BASE_URL: process.env.BASE_URL
+  },
   sassOptions: {
     prependData: `@use "./src/shared/assets/styles/core.scss";`
   }
---
+++ src/types/env.ts
@@ -0,0 +1,6 @@
+interface Env {
+  readonly PAYLOAD_CMS_URL: string;
+  readonly BASE_URL: string;
+}
+
+export type { Env };
