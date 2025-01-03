660226cb2d65e3bb4371a31fe908971691c9f8e4
K0LESAN <k0lesan2k00@gmail.com>
2025-01-03 14:33:04 +0300

    feat: add build query util

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -1,7 +1,28 @@
+import type { ObjectParams, SearchParams } from '@/types/api';
 import { locales, type Locales } from './constants';
 
 export function checkLocale(locale: string): boolean {
   return locales.some(
     (currentLocale: Locales): boolean => currentLocale === locale
   );
+}
+
+export function buildQuery(queryParams: ObjectParams): string {
+  const resultQuery: string[] = [];
+
+  for (const key in queryParams) {
+    const searchParams: SearchParams = queryParams[key];
+
+    if (Array.isArray(searchParams)) {
+      resultQuery.push(
+        `${encodeURIComponent(key)}=${encodeURIComponent(searchParams.join(','))}`
+      );
+    } else if (searchParams !== undefined) {
+      resultQuery.push(
+        `${encodeURIComponent(key)}=${encodeURIComponent(searchParams)}`
+      );
+    }
+  }
+
+  return `?${resultQuery.join('&')}`;
 }
